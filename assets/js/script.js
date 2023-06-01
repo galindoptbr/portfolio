405670ad8bb0c944d57bb971e0de62e2e7e31392
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2023-06-01 23:13:13 +0100

    changes 01 06

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Scroll to Up Button and Progress
+
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById('progress');
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = 
+        document.documentElement.scrollHeight - 
+        document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+        scrollProgress.style.display = 'grid';
+    } else {
+        scrollProgress.style.display = 'none';
+    }
+    scrollProgress.addEventListener('click', () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#FD5E36 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
