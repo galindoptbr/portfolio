57fe80c4e4c01bac2295eaef997c59eb2c0bbdbd
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2023-07-06 23:21:31 +0100

    media queyre

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,47 @@
+// Function for Mobile Menu 👇
+class MobileNavBar {
+    constructor(mobileMenu, navBar, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navBar);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavBar = new MobileNavBar(
+    ".mobile-menu",
+    ".navbar",
+    ".navbar li",
+);
+mobileNavBar.init();
+
 // Scroll to Up Button and Progress
 
 let calcScrollValue = () => {
