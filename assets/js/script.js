bc1624eeeb69f678d3b726629e1f725eb162c2d7
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2023-07-30 22:49:17 +0100

    done portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,63 @@
 // Function for Mobile Menu 👇
 class MobileNavBar {
-    constructor(mobileMenu, navBar, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navBar);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
+  constructor(mobileMenu, navBar, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navBar);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
 
-        this.handleClick = this.handleClick.bind(this);
-    }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    animateLinks() {
-        this.navLinks.forEach((link, index) => {
-            link.style.animation
-            ? (link.style.animation = "")
-            : (link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`);
-        });
-    }
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.3s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
 
-    handleClick() {
-        this.navList.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass);
-        this.animateLinks();
-    }
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
 
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
 
-    init() {
-        if (this.mobileMenu) {
-            this.addClickEvent();
-        }
-        return this;
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
     }
+    return this;
+  }
 }
 
-const mobileNavBar = new MobileNavBar(
-    ".mobile-menu",
-    ".navbar",
-    ".navbar li",
-);
+const mobileNavBar = new MobileNavBar(".mobile-menu", ".navbar", ".navbar li");
 mobileNavBar.init();
 
 // Scroll to Up Button and Progress
 
 let calcScrollValue = () => {
-    let scrollProgress = document.getElementById('progress');
-    let pos = document.documentElement.scrollTop;
-    let calcHeight = 
-        document.documentElement.scrollHeight - 
-        document.documentElement.clientHeight;
-    let scrollValue = Math.round((pos * 100) / calcHeight);
-    if (pos > 100) {
-        scrollProgress.style.display = 'grid';
-    } else {
-        scrollProgress.style.display = 'none';
-    }
-    scrollProgress.addEventListener('click', () => {
-        document.documentElement.scrollTop = 0;
-    });
-    scrollProgress.style.background = `conic-gradient(#FD5E36 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+  let scrollProgress = document.getElementById("progress");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#FD5E36 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
 };
 
 window.onscroll = calcScrollValue;
